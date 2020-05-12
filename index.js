87a0392a30b533c4640dba5e281860d3d5273d6b
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-12 13:34:16 -0400

    rebuilt the whole thing SUCCESSFULLY with readline-sync

--- index.js
+++ index.js
@@ -33,24 +33,15 @@ function getMyData() {
 //from https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/
 
 //Open the I/O stream and prompt the user for their github username
-rl.question("Please input your github username. \n", function (name) {
-  userName = name //kick that up to the global variable so we can use it later
-  // Create a new request object
-  //from: https://stackoverflow.com/questions/10341135/example-of-using-github-api-from-javascript
-  let request = new XMLHttpRequest();
-  //set event listener
-  request.onload = getMyData
-  // Initialize a request
-  request.open('get', `https://api.github.com/users/${name}`,false) //false makes this synchronous
-  // Send it
-  request.send()
-  
-  //Call the github API to get
-  //their email
-  //their profile image
-  //Close the I/O stream (important!!!)
-  rl.close()
-})
+userName = rl.question("Please input your github username. \n")
+//from: https://stackoverflow.com/questions/10341135/example-of-using-github-api-from-javascript
+let request = new XMLHttpRequest();
+//set event listener
+request.onload = getMyData
+// Initialize a request
+request.open('get', `https://api.github.com/users/${userName}`,false) //false makes this synchronous
+// Send it
+request.send()
 
 
 //Ask them some questions
@@ -63,25 +54,20 @@ let myPage = [] //needs to be a let, this variable will be VERY mutable
 
 let savedRepoName
 //Get the repo name and add a 'primary language' badge to the page
-rl.question("What is the name of your project repository? \n", function (repoName) {
-  savedRepoName = repoName //save this for the README filename
-  myPage.push(`https://img.shields.io/github/languages/top/${userName}/${repoName}`)
-  myPage.push(' ')
-  rl.close()
-})
+savedRepoName = rl.question("What is the name of your project repository? \n")
+myPage.push(`https://img.shields.io/github/languages/top/${userName}/${savedRepoName}`)
+myPage.push(' ')
 
 //Add a project title
-rl.question("What is the title of your project? \n", function (title) {
-  myPage.push(`# ${title}`)
-  myPage.push(' ')
-  rl.close()
-})
+let title = rl.question("What is the title of your project? \n")
+myPage.push(`# ${title}`)
+myPage.push(' ')
+
 //Add a project description
-rl.question("Describe your project. \n", function (desc) {
-  myPage.push(desc)
-  myPage.push(' ')
-  rl.close()
-})
+let desc = rl.question("Describe your project. \n")
+myPage.push(desc)
+myPage.push(' ')
+
 //Add a table of contents
 myPage.push(`# Table of Contents`)
 myPage.push(`## Installation`)
@@ -93,26 +79,22 @@ myPage.push(`## Questions & Contact`)
 myPage.push(' ')
 
 //Add an installation guide
-rl.question("How do you install your project? \n", function (desc) {
-  myPage.push('# Installation')
-  myPage.push(desc)
-  myPage.push(' ')
-  rl.close()
-})
+desc = rl.question("How do you install your project? \n")
+myPage.push('# Installation')
+myPage.push(desc)
+myPage.push(' ')
 
 //Add a usage guide
-rl.question("How do you use your project? \n", function (desc) {
-  myPage.push('# Usage')
-  myPage.push(desc)
-  myPage.push(' ')
-  rl.close()
-})
+desc = rl.question("How do you use your project? \n")
+myPage.push('# Usage')
+myPage.push(desc)
+myPage.push(' ')
 
 //Add a license
 myPage.push('# License')
 myPage.push(`MIT License
 
-Copyright (c) [year] [fullname]
+Copyright (c) 2020 Maxwell Roberts
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -134,22 +116,20 @@ SOFTWARE.`) //we gon find out if this preserves newlines
 myPage.push(' ')
 
 //Add a list of contributors
-rl.question("Please provide a comma-separated list of all contributors to the project. \n", function (coders) {
-  myPage.push('# Contributors')
-  myPage.push(' ')
-  aReallyGoodList = coders.split(',')
-  for(let index in aReallyGoodList){
-    myPage.push(aReallyGoodList[index])
-  }
-  rl.close()
-})
+coders = rl.question("Please provide a comma-separated list of all contributors to the project. \n")
+myPage.push('# Contributors')
+aReallyGoodList = coders.split(',')
+for(let index in aReallyGoodList){
+  myPage.push(aReallyGoodList[index])
+}
+
 //Add some example tests
-rl.question("How do you test your project? \n", function (desc) {
-  myPage.push('# Testing')
-  myPage.push(desc)
-  myPage.push(' ')
-  rl.close()
-})
+desc = rl.question("How do you test your project? \n")
+myPage.push(' ')
+myPage.push('# Testing')
+myPage.push(desc)
+myPage.push(' ')
+
 //Add a Questions? and contact section (with profile pic and email)
 myPage.push('# Questions')
 myPage.push(`![user-image](${imgURL})`)
@@ -161,7 +141,7 @@ myPage.push(' ')
 console.log(myPage)
 console.log(savedRepoName)
 
-fs.writeFile(`${savedRepoName}-README.md`, myPage.join('\n'), err => console.log(`error was thrown: ${err}`))
+fs.writeFile(`${savedRepoName}-README.md`, myPage.join('\n'), err => if(err){console.log(err)})
 //If there's an error, show the error in the console
 
 //Show some kind of message in the console, so the user knows we at least tried to create the file
